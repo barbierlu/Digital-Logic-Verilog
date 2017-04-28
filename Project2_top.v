73364c1f5e8df7622d5b4d825728fa2b0087efa8
Luke Barbier <lukebarbier12@gmail.com>
2017-04-28 17:00:08 -0600

    synchronous state change added

--- Project2_top.v
+++ Project2_top.v
@@ -1,7 +1,9 @@
-module Project_2_top (button, Clk, led, seg_sec, seg_tsec, seg_hsec, seg_msec);
+module Project_2_top (button, Clk, led, state_leds, seg_sec, seg_tsec, seg_hsec, seg_msec);
 	input [1:0] button;
 	input Clk;
 	output reg led;
+	output reg [2:0] state_leds;
+	
 	output [7:0] seg_sec;
 		assign seg_sec[7] = 0; // set the decimal point always on (active low)
 	output [6:0] seg_tsec, seg_hsec, seg_msec;
@@ -33,7 +35,7 @@ module Project_2_top (button, Clk, led, seg_sec, seg_tsec, seg_hsec, seg_msec);
 	
 	
 	
-	always @(posedge Clk, negedge button[0], negedge button[1])
+	always @(posedge Clk)
 	begin
 		if (button[1] == 0)
 			begin
@@ -90,6 +92,7 @@ module Project_2_top (button, Clk, led, seg_sec, seg_tsec, seg_hsec, seg_msec);
 					begin
 					if (wait_done)
 						leave_D <= 1;
+						
 					en_wait <=1;
 					led <= 0;
 					reset_n <= 1;
@@ -106,9 +109,9 @@ module Project_2_top (button, Clk, led, seg_sec, seg_tsec, seg_hsec, seg_msec);
 		LSRF U3 (Clk, random_num);
 		Up_Counter U5 (en_upcount, Clk_1kHz, random_num, up_count_done);
 		
-		defparam U7.top = 700; // redefine values Up_counter looks
-		defparam U7.bottom = 400;
-		Up_Counter U7 (en_wait, Clk_1kHz, 500, wait_done);
+		defparam U7.top = 2000; // redefine values Up_counter looks
+		defparam U7.bottom = 1000;
+		Up_Counter U7 (en_wait, Clk_1kHz, 1500, wait_done);
 		
 		BCD_counter U1 (Clk_1kHz, reset_n, en_bcd, dec_in_sec, dec_in_tsec, dec_in_hsec, dec_in_msec);
 		BCD_decoder U2 (dec_in_sec, dec_in_tsec, dec_in_hsec, dec_in_msec, dec_out_sec, dec_out_tsec, dec_out_hsec, dec_out_msec);
